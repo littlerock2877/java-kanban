6a0fe401a0440f992aa626237ece61e983fcaf97
Vladislav Kozlov <littlerock2877@mail.ru>
2024-06-02 17:12:13 +0300

    add: realization of FileBackedTaskManager

--- src/ru/yandex/javacource/kozlov/schedule/task/Subtask.java
+++ src/ru/yandex/javacource/kozlov/schedule/task/Subtask.java
@@ -5,15 +5,25 @@ public class Subtask extends Task {
 
     public Subtask(String name, String description, TaskStatus taskStatus, int epicId) {
         super(name, description, taskStatus);
-        this.epicId = epicId;
+        setEpicId(epicId);
     }
 
-    public int getEpicId() {
+    public Subtask(int id, String name, String description, TaskStatus taskStatus, int epicId) {
+        this(name, description, taskStatus, epicId);
+        setId(id);
+    }
+
+    public Integer getEpicId() {
         return epicId;
     }
 
     public void setEpicId(int epicId) {
         this.epicId = epicId;
+    }
+
+    @Override
+    public TaskType getType() {
+        return TaskType.SUBTASK;
     }
 
     @Override
